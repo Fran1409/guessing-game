ea975058cc0f84e39df2d8947f1feb44d2229254
Fran1409 <fran_andries@hotmail.com>
2020-11-18 09:55:35 +0100

    Add it in extra function compareGuess

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function() {
 
-    document.getElementById("submit").addEventListener("click", function() {
+    document.getElementById("submit").addEventListener("click", compareGuess);
+
+    function compareGuess(event){
         event.preventDefault();
 
         const userNumber = document.getElementById("number").value;
         const randomNumber = getRndNumber(1, 22);
-        console.log(userNumber);
-        console.log(randomNumber);
 
         if (userNumber == randomNumber) {
             alert("Awesome! You number "+ userNumber +" was correct. You can be named many things, hungry not being one of them.")
@@ -14,7 +14,7 @@
         } else {
             alert("Bummer... You guessed "+ userNumber +" and the secret number was "+ randomNumber +".")
         }
-    });
+    };
 
     function getRndNumber(min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
