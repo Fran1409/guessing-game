c4f114a82fd30ae36ddafcd43806752f7f3b5434
Fran1409 <fran_andries@hotmail.com>
2020-11-18 09:47:12 +0100

    Add alert windows for if

--- script.js
+++ script.js
@@ -7,6 +7,13 @@
         const randomNumber = getRndNumber(1, 22);
         console.log(userNumber);
         console.log(randomNumber);
+
+        if (userNumber == randomNumber) {
+            alert("Awesome! You number "+ userNumber +" was correct. You can be named many things, hungry not being one of them.")
+            
+        } else {
+            alert("Bummer... You guessed "+ userNumber +" and the secret number was "+ randomNumber +".")
+        }
     });
 
     function getRndNumber(min, max) {
